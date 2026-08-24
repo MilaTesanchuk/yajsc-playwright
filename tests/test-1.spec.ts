27af9e1df1def3e0f08dde85e7035ff21559bcb4
Mila <tesanchuk@codevog.com>
2026-08-24 22:31:54 +0300

    Unit-10: fixes

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('log in', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com/auth/login');
+    await page.goto('/auth/login');
 
     // Log in: fill in email
-    await page.getByLabel('Email address *').fill('customer@practicesoftwaretesting.com');
+    await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
     // Log in: fill in password
-    await page.getByLabel('Password *').fill('welcome01');
+    await page.locator('[data-test="password"]').fill('welcome01');
     // Log in
-    await page.locator('.btnSubmit').click();
+    await page.locator('[data-test="login-submit"]').click();
     //Expect URL
     await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
     // Expect page to have  title
-    await expect(page.locator('h1')).toHaveText('My account');
+    await expect(page.locator('[data-test="page-title"]')).toHaveText('My account');
     // Expect user name is present in the menu
-    await expect(page.locator('#menu')).toHaveText('Jane Doe');
+    await expect(page.locator('[data-test="nav-menu"]')).toHaveText('Jane Doe');
 });
