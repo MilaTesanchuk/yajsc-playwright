aae6cb82b4130ad300e3bb091b12e182102c1767
Mila <tesanchuk@codevog.com>
2026-08-26 22:04:02 +0300

    The same fix, which i deleted while merge

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -4,15 +4,15 @@ test('log in', async ({ page }) => {
     await page.goto('/auth/login');
 
     // Log in: fill in email
-    await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
+    await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
     // Log in: fill in password
-    await page.locator('[data-test="password"]').fill('welcome01');
+    await page.getByTestId('password').fill('welcome01');
     // Log in
-    await page.locator('[data-test="login-submit"]').click();
+    await page.getByTestId('login-submit').click();
     //Expect URL
     await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
     // Expect page to have  title
-    await expect(page.locator('[data-test="page-title"]')).toHaveText('My account');
+    await expect(page.getByTestId('page-title')).toHaveText('My account');
     // Expect user name is present in the menu
-    await expect(page.locator('[data-test="nav-menu"]')).toHaveText('Jane Doe');
+    await expect(page.getByTestId('nav-menu')).toHaveText('Jane Doe');
 });
